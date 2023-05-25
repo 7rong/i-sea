6c37840466de23a410a95a20480c68add5d0ffd9
cirong <7ronggg@gmail.com>
2023-05-25 14:45:11 +0800

    登入頁面api串接

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,5 +1,58 @@
 <template>
-    <div>
-        login
+    <div class="container mt-5">
+        <form class="row justify-content-center"
+        @submit.prevent="signIn">
+        <div class="col-md-6">
+            <h1 class="h3 mb-3 font-weight-normal">請先登入</h1>
+            <div class="mb-2">
+            <label for="input-email" class="sr-only">Email address
+            <input
+                type="email"
+                id="inputE-email"
+                class="form-control"
+                placeholder="Email address"
+                required
+                v-model="user.username"
+            /></label>
+            </div>
+            <div class="mb-2">
+            <label for="input-password" class="sr-only">Password
+            <input
+                type="password"
+                id="input-password"
+                class="form-control"
+                placeholder="Password"
+                required
+                v-model="user.password"
+            /></label>
+            </div>
+            <div class="text-end mt-4">
+            <button class="btn btn-lg btn-primary btn-block" type="submit">登入</button>
+            </div>
+        </div>
+        </form>
     </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      user: {
+        username: '',
+        password: '',
+      },
+    };
+  },
+  methods: {
+    signIn() {
+      const api = `${process.env.VUE_APP_API}admin/signin`;
+      // console.log(api);
+      this.$http.post(api, this.user)
+        .then((res) => {
+          console.log(res);
+        });
+    },
+  },
+};
+</script>
