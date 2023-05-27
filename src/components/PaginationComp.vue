0c82ba8defa4518b0e725f1258d8f878c76422e9
cirong <7ronggg@gmail.com>
2023-05-27 12:38:14 +0800

    加入前一頁及下一頁控制

--- src/components/PaginationComp.vue
+++ src/components/PaginationComp.vue
@@ -1,8 +1,10 @@
 <template>
   <nav aria-label="Page navigation">
     <ul class="pagination justify-content-center">
-      <li class="page-item">
-        <a class="page-link" href="#" aria-label="Previous">
+      <li class="page-item"
+      :class="{'disabled': !pages.has_pre}">
+        <a class="page-link" href="#" aria-label="Previous"
+        @click.prevent="updatePage(pages.current_page - 1)">
           <span aria-hidden="true">&laquo;</span>
         </a>
       </li>
@@ -12,8 +14,10 @@
         :class="{'active': page === pages.current_page}"
         >{{ page }}</a>
       </li>
-      <li class="page-item">
-        <a class="page-link" href="#" aria-label="Next">
+      <li class="page-item"
+      :class="{'disabled': !pages.has_next}">
+        <a class="page-link" href="#" aria-label="Next"
+        @click.prevent="updatePage(pages.current_page + 1)">
           <span aria-hidden="true">&raquo;</span>
         </a>
       </li>
