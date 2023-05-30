89aeec45d95d6f1967c5696232f38a530a8f179a
cirong <7ronggg@gmail.com>
2023-05-30 10:45:51 +0800

    建立訂單頁面

---
+++ src/views/UserOrder.vue
@@ -0,0 +1,21 @@
+<template>
+  order
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        user: {
+          name: '',
+          email: '',
+          tel: '',
+          address: '',
+        },
+        message: '',
+      },
+    };
+  },
+};
+</script>
