5cf86f048a21c3766306ba8be92cd0f2cbd774a2
cirong <7ronggg@gmail.com>
2023-06-02 10:11:40 +0800

    調整主色

--- src/views/UserCartOrder.vue
+++ src/views/UserCartOrder.vue
@@ -4,7 +4,7 @@
       <v-form v-slot="{ errors }" @submit="createOrder">
         <div class="mb-3">
           <!-- eslint-disable-next-line jsx-a11y/label-has-associated-control -->
-          <label htmlFor="email" class="form-label">Email</label>
+          <!-- <label htmlFor="email" class="form-label">Email</label> -->
           <v-field id="email"
             name="email"
             type="email"
@@ -15,7 +15,7 @@
           <error-message name="email" class="invalid-feedback"></error-message>
         </div>
         <div class="mb-3">
-          <label for="name" class="form-label">姓名</label>
+          <!-- <label for="name" class="form-label">姓名</label> -->
           <v-field id="name" name="姓名" type="text" class="form-control"
             :class="{ 'is-invalid': errors['姓名'] }"
             placeholder="王小美" rules="required"
@@ -23,7 +23,7 @@
           <error-message name="姓名" class="invalid-feedback"></error-message>
         </div>
         <div class="mb-3">
-          <label for="tel" class="form-label">電話</label>
+          <!-- <label for="tel" class="form-label">電話</label> -->
           <v-field id="tel" name="電話" type="tel" class="form-control"
                    :class="{ 'is-invalid': errors['電話'] }"
                    placeholder="0987123456" :rules="isPhone"
@@ -31,7 +31,7 @@
           <error-message name="電話" class="invalid-feedback"></error-message>
         </div>
         <div class="mb-3">
-          <label for="address" class="form-label">地址</label>
+          <!-- <label for="address" class="form-label">地址</label> -->
           <v-field id="address" name="地址" type="text" class="form-control"
                    :class="{ 'is-invalid': errors['地址'] }"
                    placeholder="請輸入地址" rules="required"
@@ -39,9 +39,9 @@
           <error-message name="地址" class="invalid-feedback"></error-message>
         </div>
         <div class="mb-3">
-          <label for="message" class="form-label">留言</label>
+          <label for="message" class="form-label">留言
           <textarea name="" id="message" class="form-control" cols="30" rows="10"
-            v-model="form.message"></textarea>
+            v-model="form.message"></textarea></label>
         </div>
         <div class="text-end">
           <button class="btn btn-danger">送出訂單</button>
