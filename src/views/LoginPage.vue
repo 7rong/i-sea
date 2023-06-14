a3d63fa92b8e3a4687eea44ecc982bfff7b06dd9
cirong <7ronggg@gmail.com>
2023-06-14 16:06:39 +0800

    管理員登入頁面

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,37 +1,39 @@
 <template>
-    <div class="container mt-5">
-        <form class="row justify-content-center"
-        @submit.prevent="signIn">
-        <div class="col-md-6">
-            <h1 class="h3 mb-3 font-weight-normal">請先登入</h1>
-            <div class="mb-2">
-            <label for="input-email" class="sr-only">Email address
-            <input
-                type="email"
-                id="inputE-email"
-                class="form-control"
-                placeholder="Email address"
-                required
-                v-model="user.username"
-            /></label>
-            </div>
-            <div class="mb-2">
-            <label for="input-password" class="sr-only">Password
-            <input
-                type="password"
-                id="input-password"
-                class="form-control"
-                placeholder="Password"
-                required
-                v-model="user.password"
-            /></label>
-            </div>
-            <div class="text-end mt-4">
-            <button class="btn btn-lg btn-primary btn-block" type="submit">登入</button>
-            </div>
+  <div class="login-banner bg-style">
+    <div class="container">
+      <form class="row justify-content-center align-items-center"
+      style="height: 100vh;"
+      @submit.prevent="signIn">
+        <div class="col-md-6 p-3 cus-bg-white rounded shadow">
+          <h1 class="h3 mb-3 font-weight-normal text-center">管理員登入</h1>
+          <div class="mb-2">
+          <label for="input-email" class="sr-only">電子郵件</label>
+          <input
+              type="email"
+              id="inputE-email"
+              class="form-control"
+              placeholder="service@isea.com"
+              required
+              v-model="user.username"
+          />
+          </div>
+          <div class="mb-2">
+          <label for="input-password" class="sr-only">密碼</label>
+          <input
+              type="password"
+              id="input-password"
+              class="form-control"
+              required
+              v-model="user.password"
+          />
+          </div>
+          <div class="text-end mt-4">
+          <button class="btn btn-lg btn-primary btn-block" type="submit">登入</button>
+          </div>
         </div>
-        </form>
+      </form>
     </div>
+  </div>
 </template>
 
 <script>
